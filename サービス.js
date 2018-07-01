2072248dcacfad28127260216f09000e628c5cf0
open-novel <32639927+open-novel@users.noreply.github.com>
2018-07-01 21:40:01 +0900

    t

--- サービス.js
+++ サービス.js
@@ -4,5 +4,14 @@ http://creativecommons.org/publicdomain/zero/1.0
 */
 
 self.addEventListener( 'fetch', e => {
+	let req = e.request
+	let path = new URL( req.url ).pathname
+	console.log( path )
+	if ( path != '/index.html' ) return
+	console.log( '!!!' )
+} )
 
+self.addEventListener( 'activate', e => {
+	console.log( 'acti' )
+	self.registration.update( )
 } )
