e822c3194e491a21eeaca87821bc149c91e2fd4d
open-novel <32639927+open-novel@users.noreply.github.com>
2018-07-22 21:02:58 +0900

    t

---
+++ サービス.js
@@ -0,0 +1,39 @@
+/*
+These codes are licensed under CC0.
+http://creativecommons.org/publicdomain/zero/1.0
+*/
+
+self.addEventListener( 'fetch', e => {
+	let req = e.request
+
+	// let path = new URL( req.url ).pathname
+	// if ( path != '/index.html' && path != '/' ) {
+	// 	 e.respondWith( fetch( req ) )
+	// 	return
+	// }
+	// console.log( '!!!' )
+	// e.respondWith(
+	// 	caches.match( req ).then( res => {
+	// 		return fetch( req ).then( res => {
+	// 			return caches.open( 'v1' ).then( cache => {
+	// 				cache.put( req, res.clone( ) )
+	// 				return res
+	// 			})
+	// 		}, err => res )
+	// 	})
+	// )
+	e.respondWith(
+		fetch( req ).then( res => {
+			caches.open( 'v1' ).then( cache => cache.put( req, res ) )
+			return res.clone( )
+		}, err => caches.match( req ) )
+	)
+} )
+
+self.addEventListener( 'install', e => {
+	self.skipWaiting( )
+} )
+
+self.addEventListener( 'activate', e => {
+	e.waitUntil( self.clients.claim( ) )
+} )
