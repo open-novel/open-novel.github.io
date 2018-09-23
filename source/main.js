80cc9372c50de11597800c3110bfe68f86faa1f0
ナンダカフラリ <nandaka.furari@gmail.com>
2018-09-23 18:01:23 +0900

    普通のホームページのようにするための材料と基本コードを追加

---
+++ source/main.js
@@ -0,0 +1,44 @@
+window.addEventListener("load", ()=>{
+	const characterPane = document.getElementById("character-pane");
+	const app = new Vue({
+		el:"#app",
+		data: {
+			chardesc: null,
+			character: {
+				name: null,
+				images:[],
+				desc:null,
+			}
+		},
+		methods: {
+			fetchCharacter: function(me,b,c) {
+				const character = me.target.innerText;
+				axios({
+					method:"GET",
+					url:`character/${character}.html`, 
+					responseType:"document",
+				}).then(response=>{
+					console.log(response);
+					const html = response.data;
+					app.character.desc = html.getElementById("description").innerHTML;
+					app.character.name = html.getElementsByTagName("title")[0].innerText;
+					app.character.images = [];
+					for(let li of html.querySelectorAll("#images > li")) {
+						console.log(li);
+						app.character.images.push(li.innerText);
+					}
+				});
+			},
+			fetchStory: function(me) {
+				const story = me.target.innerText;
+				axios({
+					method:"GET",
+					url:`story/${story}.html`,
+					responseType:"document",
+				}).then(response=>{
+					console.log(response);
+				});
+			},
+		},
+	});
+});
