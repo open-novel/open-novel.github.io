2c5fc52d464272dc64584c87b715584345b234bf
open-novel <32639927+open-novel@users.noreply.github.com>
2018-10-04 23:47:34 +0900

    t

--- サービス.js
+++ サービス.js
@@ -3,7 +3,6 @@ These codes are licensed under CC0.
 http://creativecommons.org/publicdomain/zero/1.0
 */
 
-let cache
 
 self.addEventListener( 'fetch', e => {
 
@@ -13,37 +12,28 @@ self.addEventListener( 'fetch', e => {
 
 		let network = fetch( req, req.mode == 'navigate' ? undefined : { cache: 'no-cache' } )
 			.then( res => {
-				cache.put( req, res.clone( ) )
+				caches.open('v1').then( cache => cache.put( req, res.clone( ) ) )
 				return res
 			} )
 
 		let list = [ 'manifest', 'document', 'style', 'script', 'worker', 'serviceworker' ]
 
 		if ( req.mode == 'navigate' || list.includes( req.destination ) ) {
-			e.respondWith( network.catch( ( ) => cache.match( req ) ) )
+			e.respondWith( network.catch( ( ) => caches.match( req ) ) )
 		} else {
-			e.respondWith( cache.match( req ).then( v => v || network ) )
+			e.respondWith( caches.match( req ).then( v => v || network ) )
 		}
 
 	} catch ( err ) {
 
 		self.registration.update( )
 		console.error( err )
-		e.respondWith( req )
+		e.respondWith( fetch( req ) )
 
 	}
 
 } )
 
-
 self.addEventListener( 'install', ( ) => self.skipWaiting( ) )
 
-
-self.addEventListener( 'activate', e => {
-	e.waitUntil(
-		caches.open( 'v1' ).then( c => {
-			cache = c
-			return self.clients.claim( )
-		} ) 
-	)
-} )
+self.addEventListener( 'activate', e => e.waitUntil( self.clients.claim( ) ) )
