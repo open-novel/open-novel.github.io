c20257a5e711adec68e4badf35b95f1bf1d996bd
open-novel <32639927+open-novel@users.noreply.github.com>
2019-05-26 22:04:34 +0900

    Update シナリオ.mod.js

--- devel/test/シナリオ.mod.js
+++ devel/test/シナリオ.mod.js
@@ -1,4 +1,18 @@
-import * as p from '/Player_Dev/プログラム/シナリオ.js';
+import * as Scenario from '/Player_Dev/プログラム/シナリオ.js';
 import * as tester from '/tools/onp-tester.mod.js';
 
-tester.print();
+window.Scenario = Scenario
+
+function print( name, valid ) {
+	let e = document.getElementById("onp-tester")
+	if ( valid )
+		e.textContent += `test【${ name }】: OK  `;
+	else
+		e.textContent = `test【${ name }】: NG  ` + e.textContent;
+}
+
+let text, obj
+
+//空文
+text = ``
+obj = Scenario.parse( text, 'dummy_name' )
